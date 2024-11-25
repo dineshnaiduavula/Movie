9fa8664d6cd0951075ecaad71703e6e881fcc6ea
dineshnaiduavula <dineshnaiduavula@gmail.com>
2024-11-25 21:36:30 +0530

    user

--- src/app/user/item1/item1.component.ts
+++ src/app/user/item1/item1.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../../firebase.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item1.component.html',
   styleUrl: './item1.component.css'
 })
 export class Item1Component {
-
+  constructor(public memus:FirebaseService,public routes:ActivatedRoute){}
+  menudata:any=[]
+  cattype:any
+  async ngOnInit(){
+    await this.memus.getData().then((menuItems) => {
+    console.log("Menu Items: ", menuItems);
+    this.menudata=menuItems
+    }).catch((error) => {console.error("Error: ", error);});
+    this.routes.queryParamMap.subscribe(fqparams=>{
+      this.cattype=fqparams.get('cattype');
+    console.log(this.cattype)})
+   
+  }
 }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FirebaseService } from '../../firebase.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(public memus:FirebaseService){}
+  constructor(public memus:FirebaseService,public route:Router){}
+  name:any;phone:any;seat:any;
   // ngOnInit(){
   //   this.memus.getData().then((menuItems) => {
   //   console.log("Menu Items: ", menuItems);
   //   }).catch((error) => {
   //     console.error("Error: ", error);
   //   });
+  login(){
+    console.log(this.name+""+this.phone)
+    //code
+    this.route.navigate(['User/menu'])
+  }
 }
