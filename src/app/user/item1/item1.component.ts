df2d75853af9f63ccd1cdd16488094b8086f4a57
dineshnaiduavula <dineshnaiduavula@gmail.com>
2024-11-26 19:31:56 +0530

    today

--- src/app/user/item1/item1.component.ts
+++ src/app/user/item1/item1.component.ts
@@ -9,20 +9,32 @@ import { ActivatedRoute } from '@angular/router';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './item1.component.html',
-  styleUrl: './item1.component.css'
-})
+  styleUrl: './item1.component.css'})
 export class Item1Component {
   constructor(public memus:FirebaseService,public routes:ActivatedRoute){}
   menudata:any=[]
   cattype:any
+  idd=0;
   async ngOnInit(){
-    await this.memus.getData().then((menuItems) => {
-    console.log("Menu Items: ", menuItems);
-    this.menudata=menuItems
-    }).catch((error) => {console.error("Error: ", error);});
+   const z= await this.memus.getData()
+    z.forEach((doc:any)=>{
+      this.menudata.push({'name':doc.menudat.name,'price':doc.menudat.price,'id':this.idd,
+                            'category':doc.menudat.category,'value':0})
+                    this.idd+=1
+      console.log(this.menudata)})
+
+    // await this.memus.getData().then((menuItems) => {
+    // console.log("Menu Items: ", menuItems);
+    // this.menudata=menuItems
+    // })
+    // .catch((error) => {console.error("Error: ", error);});
     this.routes.queryParamMap.subscribe(fqparams=>{
       this.cattype=fqparams.get('cattype');
     console.log(this.cattype)})
-   
   }
+  add(a:Number){
+    // this.menudata[]
+  }
+  minus(a:Number){}
+  plus(a:Number){}
 }
